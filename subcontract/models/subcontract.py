0462d2957f52df5d857f75106b98b32dc2ccf5f2
ganeshlotake <ganesh@saiaipl.com>
2019-03-14 18:30:29 +0530

    Today done 15

--- subcontract/models/subcontract.py
+++ subcontract/models/subcontract.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+# Chasnm 
 
 from odoo import fields, models, api, _
 from odoo.exceptions import UserError
