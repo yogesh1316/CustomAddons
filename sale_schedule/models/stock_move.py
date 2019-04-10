fbcd4e6426750b844a99755796cac517cc65da9a
JeevanGang <g1gangarde@gmail.com>
2019-04-10 11:25:05 +0530

    Scheduled date added in _assign_picking

--- sale_schedule/models/stock_move.py
+++ sale_schedule/models/stock_move.py
@@ -95,8 +95,4 @@ class StockMove(models.Model):
             # record won't be found.
             if recompute:
                 move.recompute()
-<<<<<<< HEAD
         return True
-=======
-        return True
->>>>>>> eb17fd60fcf6bf4c66e4010e3b8d093aa7c127a1
