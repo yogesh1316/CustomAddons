f0ca90d4295b8e2be9aa538fa31bdb83d9c50d62
chandrahyd <waghmarec0179@gmail.com>
2019-05-06 15:46:59 +0530

    delivery challan report

---
+++ subcontract_delivery_challan/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "subcontract_delivery_challan",
+
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+
+    
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','stock'],
+
+    # always loaded
+    'data': [
+        'report/stock_picking_main_report.xml',
+        'report/stock_picking_report_menu.xml',
+        'report/template_demo.xml'
+    ],
+    
+    
+}
