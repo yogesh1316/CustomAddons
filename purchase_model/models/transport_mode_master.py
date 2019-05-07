b88a8c3abf8040cffe438f441ffb15428d60e3ea
yogesh1316 <yogeshchaudhari1316@outlook.com>
2019-05-07 14:04:09 +0530

    Purchase Model Done Upto Indent Type Sequence

--- purchase_model/models/transport_mode_master.py
+++ purchase_model/models/transport_mode_master.py
@@ -7,6 +7,7 @@ from odoo.exceptions import UserError
 
 class transport_mode_master(models.Model):
     _name='transport_mode.master'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
     _rec_name ="transport_mode_desc"  
     _description="Transport Mode Master"
     
